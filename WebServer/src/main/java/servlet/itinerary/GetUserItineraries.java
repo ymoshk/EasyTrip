25002e488abdaba23bb103261b573d22e31ae01c
sahar <saharlaila@gmail.com>
2021-08-24 18:47:36 +0300

    Sahar test

--- WebServer/src/main/java/servlet/itinerary/GetUserItineraries.java
+++ WebServer/src/main/java/servlet/itinerary/GetUserItineraries.java
@@ -1,11 +1,14 @@
 package servlet.itinerary;
 
+import cache.ItineraryCache;
 import com.google.gson.Gson;
 import connection.DataEngine;
 import itinerary.Itinerary;
+import itinerary.QuestionsData;
 import log.LogsManager;
 import model.flightOffer.FlightOffer;
 import model.itinerary.ItineraryModel;
+import model.itinerary.ItineraryStatus;
 import util.Utils;
 
 import javax.servlet.ServletException;
@@ -28,11 +31,31 @@ public class GetUserItineraries extends HttpServlet {
 
         try (PrintWriter out = resp.getWriter()) {
             List<ItineraryModel> itineraries = DataEngine.getInstance().getUserItineraries(userId);
-            out.println(new Gson().toJson(itineraries));
+
+            List<ItineraryAndStatus> res =
+                    (List<ItineraryAndStatus>) itineraries.stream().
+                            map(itineraryModel ->
+                            {
+                                Itinerary itinerary = new Gson().fromJson(itineraryModel.getJsonData(), Itinerary.class);
+                                return new ItineraryAndStatus(itinerary.getQuestionsData(), itineraryModel.getStatus(), itinerary.getItineraryId());
+                            });
+
+            out.println(new Gson().toJson(res));
         } catch (Exception e) {
             LogsManager.logException(e);
             resp.setStatus(500);
         }
+    }
 
+    private class ItineraryAndStatus {
+        QuestionsData questionsData;
+        model.itinerary.ItineraryStatus itineraryStatus;
+        String itineraryId;
+
+        private ItineraryAndStatus(QuestionsData questionsData, ItineraryStatus itineraryStatus, String itineraryId) {
+            this.questionsData = questionsData;
+            this.itineraryStatus = itineraryStatus;
+            this.itineraryId = itineraryId;
+        }
     }
 }
