0c5432cb2b57ff1f04538d7a06186f443744ac55
BarMa <barmalkov@gmail.com>
2021-08-31 18:46:49 +0300

    changed attraction fetching

--- API/src/main/java/util/google/GoogleMapsApiUtils.java
+++ API/src/main/java/util/google/GoogleMapsApiUtils.java
@@ -6,29 +6,35 @@ import container.PriceRange;
 
 public class GoogleMapsApiUtils {
     public static int RADIUS = 50 * 1000;
-    public static int RESTAURANT_RADIUS = 5 * 1000;
+    public static int ATTRACTION_RADIUS = 6 * 1000;
     public static TextSearchRequest getTextSearchRequest(GeoApiContext context, String attractionName, String cityName,
                                                          LatLng cityCenter, PriceRange priceRange, PlaceType type, PriceLevel priceLevel) {
         if(type.equals(PlaceType.GROCERY_OR_SUPERMARKET)) {
-            return PlacesApi.textSearchQuery(context, "market" + "+" + cityName);
+            return PlacesApi.textSearchQuery(context, "market" + "+" + cityName).
+                    location(cityCenter).radius(ATTRACTION_RADIUS).rankby(RankBy.PROMINENCE);
         }
         // DOCTOR == BEACH
         else if(type.equals(PlaceType.DOCTOR)) {
-            return PlacesApi.textSearchQuery(context, "beach" + "+" + cityName);
+            return PlacesApi.textSearchQuery(context, "beach" + "+" + cityName).
+                    location(cityCenter).radius(ATTRACTION_RADIUS).rankby(RankBy.PROMINENCE);
         }
         // ATM == TOP SIGHT
         else if(type.equals(PlaceType.ATM)) {
-            return PlacesApi.textSearchQuery(context, "top sights" + "+" + cityName);
+            return PlacesApi.textSearchQuery(context, "top sights" + "+" + cityName).
+                    location(cityCenter).radius(ATTRACTION_RADIUS).rankby(RankBy.PROMINENCE);
         }
         //fetch amusement parks without a radius limit
         else if(type.equals(PlaceType.AMUSEMENT_PARK)){
             return PlacesApi.textSearchQuery(context, "amusement park" + "+" + cityName).type(type);
         }
         else if(type.equals(PlaceType.RESTAURANT)){
-            return PlacesApi.textSearchQuery(context, attractionName).type(type).minPrice(priceLevel).maxPrice(priceLevel);
+            return PlacesApi.textSearchQuery(context, attractionName).
+                    type(type).location(cityCenter).radius(ATTRACTION_RADIUS).
+                    minPrice(priceLevel).maxPrice(priceLevel);
         }
 
-        return PlacesApi.textSearchQuery(context, attractionName).type(type);
+        return PlacesApi.textSearchQuery(context, attractionName).
+                type(type).location(cityCenter).radius(ATTRACTION_RADIUS).rankby(RankBy.PROMINENCE);
     }
 
     public static TextSearchRequest getNextPageTextSearchRequest(GeoApiContext context, String pageToken) {
@@ -45,6 +51,6 @@ public class GoogleMapsApiUtils {
     }
 
     public static NearbySearchRequest getNearByPlaces(GeoApiContext context, LatLng location){
-        return PlacesApi.nearbySearchQuery(context, location).type(PlaceType.RESTAURANT).radius(RESTAURANT_RADIUS);
+        return PlacesApi.nearbySearchQuery(context, location).type(PlaceType.RESTAURANT).radius(ATTRACTION_RADIUS);
     }
 }
